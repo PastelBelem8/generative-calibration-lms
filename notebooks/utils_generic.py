6bc6c1cf58e902d29e97ccc100c1f231e1242bfb
Catarina Belem <cbelem@uci.edu>
2022-03-01 17:23:05 -0800

    Add utils_generic.generate_uuid
    Given an iterable (e.g, dict or list) it generates a unique identifier
    by hashing its content.

--- notebooks/utils_generic.py
+++ notebooks/utils_generic.py
@@ -1,6 +1,20 @@
 import inspect
+import json
+import hashlib
 
 
 def filter_params(params: dict, method: callable) -> dict:
     method_params = inspect.signature(method).parameters
     return {p: p_val for p, p_val in params.items() if p in method_params}
+
+
+def filter_params_by_prefix(params: dict, prefix: str) -> dict:
+    return {p: p_val for p, p_val in params.items() if p.startswith(prefix)}
+
+
+def generate_uuid(content, indent: int=2) -> str:
+    """Deterministic uuid generator of the `content`."""
+    content = json\
+        .dumps(content, sort_keys=True, indent=indent)\
+        .encode("utf-8")
+    return hashlib.md5(content).hexdigest()
